4b73cf3935ea6ee9bbe86889c274a5d62e329deb
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-07-31 17:12:43 +0300

    start FamiliesAreOurPriority component

---
+++ src/components/FamiliesAreOurPriority.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BsFillPeopleFill } from "react-icons/bs";
+
+export default function FamiliesAreOurPriority() {
+  return (
+    <div className="families-are-our-priority">
+      <div className="icon">
+        <BsFillPeopleFill />
+      </div>
+      <h1>Families are our priority</h1>
+      <h2>
+        Curabitur eget aliquam elit. In mauris purus, auctor a eleifend non,
+        placerat vel arcu. In non consectetur lorem. Morbi non varius sapien
+        suscipit mauri.
+      </h2>
+      <button>Get started</button>
+    </div>
+  );
+}
