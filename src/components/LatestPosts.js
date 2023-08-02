71a672a47d88cf05b4c019fad56f07662256a096
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-02 19:57:49 +0300

    start LatestPosts component

---
+++ src/components/LatestPosts.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function LatestPosts() {
+  return (
+    <div className="latest-posts">
+      <h1>Latest posts</h1>
+      <h2>
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae.
+        <br /> Praesent efficitur nibh massa morbi sagittis ornare dui in
+        ornare.
+      </h2>
+      <button className="view-all-button">View all</button>
+      
+    </div>
+  );
+}
