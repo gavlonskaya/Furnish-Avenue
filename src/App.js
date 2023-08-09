9e366548056e5c7f5b8b483cb529e309a5bc956a
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-09 16:36:08 +0300

    start what-customers-say-component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,38 @@
 import React from "react";
-import Blog from "./components/Blog";
-
+import WhatCustomersSay from "./components/WhatCustomersSay";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       orders: [],
-
-      blogItems: [
+      sayItems: [
         {
           id: 1,
-          img: "the-9-best-homes-in-new-york.jpg",
-          title: "The 9 best homes in New York",
-          date: "May 10, 2023",
+          stars: 5,
           description:
-            "Integer tincidunt rutrum faucibus. Proin sit amet varius arcu. Aliquam vel leo augue donec.",
-          category: "Trends",
+            "Aliquam erat volutpat. Nulla pretium nec urna et convallis. Ut varius mi lacus, nec molestie libero ultricies nec. Aenean lacinia dui elit, ut gravida quam.",
+          name: "Michael Webb",
+          category: "Customer",
+          img: "michael-webb.jpg",
         },
         {
           id: 2,
-          img: "how-to-easily-buy-a-house-with-realco.jpg",
-          title: "How to easily buy a house with Realco",
-          date: "May 10, 2023",
+          stars: 3,
           description:
-            "Praesent nec felis nisl. Phasellus eget lacus a metus fringilla fermentum sit amet.",
-          category: "DIY",
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi pretium iaculis ultricies. Aenean in leo vitae tortor.",
+          name: "Amber Keene",
+          category: "Customer",
+          img: "amber-keene.jpg",
         },
         {
           id: 3,
-          img: "renting-houses-complete-guide.jpg",
-          title: "Renting houses - complete guide",
-          date: "May 10, 2023",
+          stars: 4,
           description:
-            "Nunc in bibendum leo, gravida scelerisque quam nulla interdum tellus a turpis.",
-          category: "DIY",
-        },
-        {
-          id: 4,
-          img: "renting-houses-complete-guide.jpg",
-          title: "Renting houses - complete guide",
-          date: "May 10, 2023",
-          description:
-            "Nunc in bibendum leo, gravida scelerisque quam nulla interdum tellus a turpis.",
-          category: "DIY",
-        },
-        {
-          id: 5,
-          img: "renting-houses-complete-guide.jpg",
-          title: "Renting houses - complete guide",
-          date: "May 10, 2023",
-          description:
-            "Nunc in bibendum leo, gravida scelerisque quam nulla interdum tellus a turpis.",
-          category: "DIY",
-        },
-        {
-          id: 6,
-          img: "renting-houses-complete-guide.jpg",
-          title: "Renting houses - complete guide",
-          date: "May 10, 2023",
-          description:
-            "Nunc in bibendum leo, gravida scelerisque quam nulla interdum tellus a turpis.",
-          category: "DIY",
+            "Condimentum viverra orci. Pellentesque suscipit odio nisl, non ultricies purus mattis eget. In placerat, lorem a sodales ullamcorper, eros nibh laoreet nisl.",
+          name: "Sarah Tarleton",
+          category: "Customer",
+          img: "sarah-tarleton.jpg",
         },
       ],
     };
@@ -70,7 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <Blog blogItems={this.state.blogItems} />
+        <WhatCustomersSay sayItems={this.state.sayItems} />
       </div>
     );
   }
