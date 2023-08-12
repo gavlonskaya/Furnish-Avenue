951b7f227d808dd05a9e406d88358ee8085cd333
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-12 14:12:42 +0300

    adding BEM methodology

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { MdHouse } from "react-icons/md";
 import { MdReceipt } from "react-icons/md";
 import { MdKey } from "react-icons/md";
@@ -6,40 +6,40 @@ import { MdKey } from "react-icons/md";
 export default function HowItWorks() {
   return (
     <div className="how-it-works">
-      <div className="title">
-        <h1>How It Works</h1>
-        <h2>
+      <div className="how-it-works__title">
+        <h1 className="how-it-works__title-heading">How It Works</h1>
+        <h2 className="how-it-works__title-subheading">
           Nunc laoreet consequat nunc at consequat. Praesent aliquam mi id
           efficitur pellentesque. Maecenas varius felis felis.
         </h2>
       </div>
-      <div className="items">
-        <div className="item">
-          <div className="icon">
-            <MdHouse />
+      <div className="how-it-works__items">
+        <div className="how-it-works__item">
+          <div className="how-it-works__item-icon">
+            <MdHouse className="how-it-works__item-icon-svg" />
           </div>
-          <h2>Find property</h2>
-          <h3>
+          <h2 className="how-it-works__item-heading">Find property</h2>
+          <h3 className="how-it-works__item-subheading">
             Odales mauris quis tellus facilisis, vel mattis magna interdum.
             Curabitur eget aliquam elit. In mauris purus, auctor a eleifend non.
           </h3>
         </div>
-        <div className="item">
-          <div className="icon">
-            <MdReceipt />
+        <div className="how-it-works__item">
+          <div className="how-it-works__item-icon">
+            <MdReceipt className="how-it-works__item-icon-svg" />
           </div>
-          <h2>Make a deal</h2>
-          <h3>
+          <h2 className="how-it-works__item-heading">Make a deal</h2>
+          <h3 className="how-it-works__item-subheading">
             Pellentesque suscipit mauris. Nam dictum, erat a volutpat varius,
             urna nisi sollicitudin orci, sagittis facilisis odio tortor ac mi.
           </h3>
         </div>
-        <div className="item">
-          <div className="icon">
-            <MdKey />
+        <div className="how-it-works__item">
+          <div className="how-it-works__item-icon">
+            <MdKey className="how-it-works__item-icon-svg" />
           </div>
-          <h2>Get your keys</h2>
-          <h3>
+          <h2 className="how-it-works__item-heading">Get your keys</h2>
+          <h3 className="how-it-works__item-subheading">
             Nulla vel bibendum purus. Etiam tempus quam vel pulvinar rutrum.
             Duis semper, metus vitae fermentum consequat.
           </h3>
