bc4216753a6dd0f57670431f69b9ea4dac31327e
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-12 22:10:41 +0300

    adding BEM methodology

--- src/components/TheBestHouses.js
+++ src/components/TheBestHouses.js
@@ -1,23 +1,28 @@
 import React from "react";
 import theBestHouses from "../img/the-best-houses.jpg";
-
 export default function TheBestHouses() {
   return (
     <div className="the-best-houses">
-      <div className="content">
-        <div className="right-content">
-          <h1>The best houses for family comfort</h1>
-          <p>
+      <div className="the-best-houses__content">
+        <div className="the-best-houses__right-content">
+          <h1 className="the-best-houses__title">
+            The best houses for family comfort
+          </h1>
+          <p className="the-best-houses__description">
             Ectus ipsum faucibus justo, et semper quam nisl in nibh. Proin
             sollicitudin, lorem non posuere blandit.
           </p>
-          <div className="container-button">
-            <button className="explore-properties">Explore properties</button>
-            <button className="contact-agent">Contact agent</button>
+          <div className="the-best-houses__container-button">
+            <button className="the-best-houses__explore-properties">
+              Explore properties
+            </button>
+            <button className="the-best-houses__contact-agent">
+              Contact agent
+            </button>
           </div>
         </div>
       </div>
-      <img src={theBestHouses} className="img" />
+      <img src={theBestHouses} className="the-best-houses__img" />
     </div>
   );
 }
