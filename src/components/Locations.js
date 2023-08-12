fa1468060bf2e05e0ad9a1974813bfd411dfc691
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-12 13:59:19 +0300

    adding BEM methodology

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -6,24 +6,25 @@ export class Locations extends Component {
   render() {
     return (
       <div className="locations">
-        <h1>Locations</h1>
-        <h2>
+        <h1 className="locations__title">Locations</h1>
+        <h2 className="locations__subtitle">
           Nunc mi ante, fringilla eget iaculis et, maximus vel purus integer
           lacus urna, aliquet non nisl sit amet.
         </h2>
-        <div className="locations-items">
+        <div className="locations__items">
           {this.props.locationsItems.map((el) => (
             <LocationsItem key={el.id} locationsItems={el} />
           ))}
         </div>
-        <div className="button-locations">
-          <button>Explore properties</button>
-          <button className="сontact-agent">
-            Contact agent <MdOutlineChevronRight />
+        <div className="locations__buttons">
+          <button className="locations__button">Explore properties</button>
+          <button className="locations__button locations__button--contact-agent">
+            Contact agent <MdOutlineChevronRight className="locations__button-icon" />
           </button>
         </div>
       </div>
     );
   }
 }
+
 export default Locations;
--- src/components/LocationsItem.js
+++ src/components/LocationsItem.js
@@ -5,10 +5,10 @@ export class LocationsItem extends Component {
   render() {
     return (
       <div className="locations-item">
-        <h1>
+        <h1 className="locations-item__title">
           {this.props.locationsItems.city}, {this.props.locationsItems.state}
         </h1>
-        <button className="view-properties">
+        <button className="locations-item__button">
           View Properties <MdOutlineChevronRight />
         </button>
       </div>
