cbee0959aaed566da6a0ede23dde8697f4927410
Gavlonskaya Ekaterina <gavlonskaya.katerina.yandex.ru>
2023-08-14 15:08:41 +0300

    adding BEM methodology

--- src/components/MagazinesAndMedia.js
+++ src/components/MagazinesAndMedia.js
@@ -8,12 +8,36 @@ import logoipsum5 from "../img/logoipsum-5.jpg";
 export default function MagazinesAndMedia() {
   return (
     <div className="magazines-and-media">
-      <h1>As seen on the world's best<br/>magazines and media</h1>
-      <img src={logoipsum1} alt="logoipsum" />
-      <img src={logoipsum2} alt="logoipsum" />
-      <img src={logoipsum3} alt="logoipsum" />
-      <img src={logoipsum4} alt="logoipsum" />
-      <img src={logoipsum5} alt="logoipsum" />
+      <h1 className="magazines-and-media__title">
+        As seen on the world's best
+        <br />
+        magazines and media
+      </h1>
+      <img
+        className="magazines-and-media__logo"
+        src={logoipsum1}
+        alt="logoipsum"
+      />
+      <img
+        className="magazines-and-media__logo"
+        src={logoipsum2}
+        alt="logoipsum"
+      />
+      <img
+        className="magazines-and-media__logo"
+        src={logoipsum3}
+        alt="logoipsum"
+      />
+      <img
+        className="magazines-and-media__logo"
+        src={logoipsum4}
+        alt="logoipsum"
+      />
+      <img
+        className="magazines-and-media__logo"
+        src={logoipsum5}
+        alt="logoipsum"
+      />
     </div>
   );
 }
